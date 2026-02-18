22a6c588451a6e5aae52f8f698b4080a2d42d69f
sreehithareddypuli05 <sreehithareddypuli@gmail.com>
2026-02-18 19:16:39 +0100

    Update settings.py

--- sonar_project/settings.py
+++ sonar_project/settings.py
@@ -5,7 +5,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this-in-production-abc123xyz')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+
+ALLOWED_HOSTS = [
+    "sonar-rock-vs-mine-project.onrender.com",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
